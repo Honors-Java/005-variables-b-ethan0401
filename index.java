dc9bb32f7d724915293d7a57a8882aa52943adc9
ethan0401 <71141660+ethan0401@users.noreply.github.com>
2020-10-16 00:10:57 +0000

    i mad this thing go off the screen

--- index.java
+++ index.java
@@ -1,6 +1,15 @@
 void setup() {
 	size(500, 500);
 }
+{
+x = 20
+y = 300
+z = 0
+t = 255
+a = 145
+o = 170
+}
+
 
 void draw() {
 // Step 1: Write code that draws the following screenshots 
@@ -16,10 +25,16 @@ void draw() {
 
 // Step 4: Make it so when the program runs the ball slides off the screen exiting at the point (500, 500)
 
-  background(255);
-  stroke(0);
-
-
+  background(t);
+  stroke(z);
   
-
+  //grey square
+  fill(z, z, t);
+  rect(x, x, y, y);
+  //white circle
+  fill(t, t, z);
+  ellipse(o, o, a, a);
+ // move thing
+ o = o + 2
+ x = x + 2
 }
