f170b5946e081fcecc631b92defa1ea9a3197464
ethan0401 <71141660+ethan0401@users.noreply.github.com>
2020-10-20 14:54:32 +0000

    i made random functions for the ellipse and the rect

--- index.java
+++ index.java
@@ -1,17 +1,18 @@
+int xPosition = 100;
+int yPosition = 100;
+int rectWidth = 200;
+int rectHeight = 200;
+int r = 150;
+int g = 0;
+int b = 0; 
+
 void setup() {
 	size(500, 500);
+  frameRate(30);
 }
-{
-x = 20
-y = 300
-z = 0
-t = 255
-a = 145
-o = 170
-}
-
 
 void draw() {
+
 // Step 1: Write code that draws the following screenshots 
 // with hard-coded values. (Feel free to use colors 
 // instead of grayscale.)
@@ -24,17 +25,23 @@ void draw() {
 // For example, “variable1 = variable1 + 2;”. 
 
 // Step 4: Make it so when the program runs the ball slides off the screen exiting at the point (500, 500)
+//background(255);
+stroke(0);
 
-  background(t);
-  stroke(z);
-  
-  //grey square
-  fill(z, z, t);
-  rect(x, x, y, y);
-  //white circle
-  fill(t, t, z);
-  ellipse(o, o, a, a);
- // move thing
- o = o + 2
- x = x + 2
+fill(r, g, b);
+rect(xPosition, yPosition, rectWidth, rectHeight);
+fill((int) random(0, 255), (int) random(0, 255), (int)random(0, 255));
+ellipse(mouseX + (int) random(-30, 30), mouseY + (int) random(-30, 30), 10, 10);
 }
+
+void mousePressed(){
+  r = (int) random(0, 255);
+  g = (int) random(0, 255);
+  b = (int) random(0, 255);
+  background(255);
+  xPosition = (int) random(0, 400);
+  yPosition = (int) random(0, 400);
+  rectWith = (int) random(50, 400);
+  rectHeight = (int) random(50, 400);
+}
+ 
